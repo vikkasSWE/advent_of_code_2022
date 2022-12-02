8368bb2d8def86698bdefdd3b342e2308a3d1ce3
Viktor Kasimir <kasimir.viktor@gmail.com>
2022-12-02 07:01:39 +0100

    part 2

--- days/day_2/src/main.rs
+++ days/day_2/src/main.rs
@@ -1,7 +1,5 @@
 use std::fs;
 
-fn part_2(_input: &String) {}
-
 fn main() {
     let input = fs::read_to_string("days/day_2/input.txt").unwrap();
     part_1(&input);
@@ -24,19 +22,14 @@ fn part_1(input: &String) {
         let mut result = Win::Equal;
 
         if a == "A" {
-            // rock
             if b == "X" {
-                // rock
                 result = Win::Equal;
             } else if b == "Y" {
-                // paper
                 result = Win::B;
             } else if b == "Z" {
-                // scissor
                 result = Win::A;
             }
         } else if a == "B" {
-            // paper
             if b == "X" {
                 result = Win::A;
             } else if b == "Y" {
@@ -45,7 +38,6 @@ fn part_1(input: &String) {
                 result = Win::B;
             }
         } else if a == "C" {
-            // scissor
             if b == "X" {
                 result = Win::B;
             } else if b == "Y" {
@@ -66,6 +58,63 @@ fn part_1(input: &String) {
         }
 
         match result {
+            Win::Equal => points = points + 3 + choice_point,
+            Win::A => points = points + choice_point,
+            Win::B => points = points + 6 + choice_point,
+        }
+    }
+
+    println!("{points}");
+}
+
+fn part_2(input: &String) {
+    let mut points = 0;
+    for line in input.lines() {
+        let mut round = line.split(' ');
+        let a = round.next().unwrap();
+        let b = round.next().unwrap();
+
+        let mut desired_outcome = Win::Equal;
+        if b == "X" {
+            desired_outcome = Win::A;
+        } else if b == "Y" {
+            desired_outcome = Win::Equal;
+        } else if b == "Z" {
+            desired_outcome = Win::B;
+        }
+
+        let mut choice = "";
+
+        if a == "A" {
+            match desired_outcome {
+                Win::Equal => choice = "X",
+                Win::A => choice = "Z",
+                Win::B => choice = "Y",
+            }
+        } else if a == "B" {
+            match desired_outcome {
+                Win::Equal => choice = "Y",
+                Win::A => choice = "X",
+                Win::B => choice = "Z",
+            }
+        } else if a == "C" {
+            match desired_outcome {
+                Win::Equal => choice = "Z",
+                Win::A => choice = "Y",
+                Win::B => choice = "X",
+            }
+        }
+
+        let mut choice_point = 0;
+        if choice == "X" {
+            choice_point = 1;
+        } else if choice == "Y" {
+            choice_point = 2;
+        } else if choice == "Z" {
+            choice_point = 3;
+        }
+
+        match desired_outcome {
             Win::Equal => points = points + 3 + choice_point,
             Win::A => points = points + choice_point,
             Win::B => points = points + 6 + choice_point,
