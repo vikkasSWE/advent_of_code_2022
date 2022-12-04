3a50292fa5757719e667b304e5becc9aec2f0718
Viktor Kasimir <kasimir.viktor@gmail.com>
2022-12-04 06:24:16 +0100

    part 2

--- days/day_4/src/main.rs
+++ days/day_4/src/main.rs
@@ -1,5 +1,4 @@
-use std::{collections::HashMap, fs};
-fn part_2(_input: &String) {}
+use std::fs;
 
 fn main() {
     let input = fs::read_to_string("days/day_4/input.txt").unwrap();
@@ -11,8 +10,8 @@ fn part_1(input: &String) {
     let mut sum = 0;
     for line in input.lines() {
         let mut split = line.split(",");
-        let mut sec_1 = split.next().unwrap();
-        let mut sec_2 = split.next().unwrap();
+        let sec_1 = split.next().unwrap();
+        let sec_2 = split.next().unwrap();
 
         let mut sec_1 = sec_1.split("-");
         let sec_1_start = sec_1.next().unwrap().parse::<i32>().unwrap();
@@ -28,6 +27,34 @@ fn part_1(input: &String) {
         }
 
         if sec_2_start >= sec_1_start && sec_2_end <= sec_1_end {
+            sum += 1;
+            continue;
+        }
+    }
+
+    println!("{sum}")
+}
+
+fn part_2(input: &String) {
+    let mut sum = 0;
+    for line in input.lines() {
+        let mut split = line.split(",");
+        let sec_1 = split.next().unwrap();
+        let sec_2 = split.next().unwrap();
+
+        let mut sec_1 = sec_1.split("-");
+        let sec_1_start = sec_1.next().unwrap().parse::<i32>().unwrap();
+        let sec_1_end = sec_1.next().unwrap().parse::<i32>().unwrap();
+
+        let mut sec_2 = sec_2.split("-");
+        let sec_2_start = sec_2.next().unwrap().parse::<i32>().unwrap();
+        let sec_2_end = sec_2.next().unwrap().parse::<i32>().unwrap();
+
+        if (sec_1_start >= sec_2_start && sec_1_start <= sec_2_end)
+            || (sec_1_end >= sec_2_start && sec_1_end <= sec_2_end)
+            || (sec_2_start >= sec_1_start && sec_2_start <= sec_1_end)
+            || (sec_2_end >= sec_1_start && sec_2_end <= sec_1_end)
+        {
             sum += 1;
             continue;
         }
